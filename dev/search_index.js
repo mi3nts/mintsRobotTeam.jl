4cc92763cd20e8de106cb7b4e509082b29aadc77
Documenter.jl <documenter@juliadocs.github.io>
2022-06-03 12:52:48 +0000

    build based on 94593b7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = mintsRobotTeam","category":"page"},{"location":"#mintsRobotTeam","page":"Home","title":"mintsRobotTeam","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for mintsRobotTeam.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [mintsRobotTeam]","category":"page"}]
+}
